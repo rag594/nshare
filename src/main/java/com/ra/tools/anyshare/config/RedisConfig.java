9ac190a48ed61dea85b056b78e91d7dad08b4152
rag594 <raghavrastogi594@gmail.com>
2019-09-10 19:23:58 +0530

    retrieve REDISGOTO_URL from env variable

--- src/main/java/com/ra/tools/anyshare/config/RedisConfig.java
+++ src/main/java/com/ra/tools/anyshare/config/RedisConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -16,15 +17,25 @@ import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @Configuration
 @ComponentScan("com.ra.tools.anyshare")
 @EnableRedisRepositories(basePackages = "com.ra.tools.anyshare.repository")
-@PropertySource("classpath:application.properties")
 public class RedisConfig {
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        try {
+            URI redisURI = new URI(System.getenv("REDISTOGO_URL"));
+        RedisStandaloneConfiguration redisStandaloneConfiguration =
+                new RedisStandaloneConfiguration(redisURI.getHost(), redisURI.getPort());
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Bean
